c1e2588486961f5064fb01bd1a25caca2d343180
Justin <pride4carolinas@gmail.com>
2021-06-23 18:16:39 -0400

    Need to solve line 77 syntax error 202106231816

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ dealer_total = 0
 player_total = 0
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 ]
 
+
+def game_start():
+    ready = input("When you are ready to start, type deal and then press enter/return. \n")
+    if ready == "deal":
+        deal()
+    else:
+        print("Sorry you're response was not recognized")
+
 def deal():
     dealer.append(random.choice(cards))
     player.append(random.choice(cards))
@@ -13,76 +21,69 @@ def deal():
     player.append(random.choice(cards))
     print(f"The dealer's hand is {dealer}.")
     print(f"The player's hand is {player}.")
+    if sum(dealer) == 21 and sum(player) == 21:
+        print("The game is a draw!")
+        play_again()
+    elif sum(dealer) == 21 and sum(player) != 21:
+        print("The dealer has 21 and wins the game. ")
+        play_again()
+    elif sum(dealer) != 21 and sum(player) == 21:
+        print("You have 21 and win the game. ")
+        play_again()
+
+def who_wins():
+    player_total == sum(player)
+    dealer_total == sum(dealer)
+    if player_total > dealer_total:
+        print("You had the higher hand.  You win the game")
+        play_again()
+    elif player_total < dealer_total:
+        print("The dealer has the higher hand.  You loose the game")
+        play_again()
+    elif player_total == dealer_total:
+        print("The game is a draw!")
+        play_again()
+
+def play_again():
+    dealer = []
+    player = []
+    dealer_total = 0
+    player_total = 0
+    game_again = input("Would you like to play again")
+    if game_again == yes:
+        deal()
+    else:
+        print("Thank you for playing.  I hope you enjoyed the game.")
+
 
 game_over = False
 
-# def choice():
-#     decision = input("Whould you like to hit or stand? ")
-#     if decision == "hit":
-#         player.append(random.choice(cards))
-#     else:
-#         who_wins()
-
-ready = input("When you are ready to start, type deal and then press enter/return. \n")
-
-if ready == "deal":
-    deal()
-else:
-    print("Sorry you're response was not recognized")
+game_again = ""
 
 
-while game_over != True:
+
+
+game_start()
+
+while game_over == False:
     choice = input("Would you like to hit or stay? \n")
     if choice == "hit":
         player.append(random.choice(cards))
         if sum(player) >= 22:
             print(f"{player} Sorry, you are over 21 and you loose.")
-            game_over = True
-        elif choice == "stay":
-           print(player)
-    elif sum(dealer) <= 16:
+            play_again()
+        elif sum(player) <=21:
+            print(f"Your hand is now {player} "
+    elif sum(dealer) <= 15:
         dealer.append(random.choice(cards))
         if sum(dealer) >= 22:
-            printf("{dealer} The dealer has gone over 21 and you win")
-            game_over = True
+            print(f"{dealer} The dealer has gone over 21 and you win")
+            play_again()
         else:
             print(dealer)
+            who_wins()
     else:
         who_wins()
 
-# def players_totals():
-#     for n in player:
-#         player_total = sum(player)
-#     for o in dealer:
-#         dealer_total = sum(dealer)
-#     print(f"The player's total is {player_total}. ")
-#     print(f"The player's total is {dealer_total}. ")
 
-#def who_wins():
-    #
-
-
-#import art
-#print logo
-
-
-# ask play if they are ready to deal cards
-
-
-
-# issues two random cards to each player
-
-
-
-# evaluate totals and if players are over 21
-
-
-# combine dealer and player lists for total
-#evaluate to see if either is over 21
-
-# deal additional cards to each player
-
-#evaluate totals and if anyone is over 21
-
-# present the winner or declare a draw
 
